23b94cac7790bb34a7fc0f4dc37a3ff0c719e879
Shruti Srivastava <shrutishreya222@gmail.com>
2022-10-23 23:54:46 +0530

    problem 1913

---
+++ Array/1913_maximum_product_difference_between_two_pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+         Arrays.sort(nums);
+        int result=0,temp1=0,temp2=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            temp1=nums[nums.length-2]*nums[nums.length-1];
+            temp2=nums[0]*nums[1];
+            result=temp1-temp2;
+        }
+        return result;
+        
+    }
+}
