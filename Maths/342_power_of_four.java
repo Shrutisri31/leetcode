08dabfacedcfa2cb2f19ce87f62bd051800f72ec
Shruti Srivastava <shrutishreya222@gmail.com>
2022-11-10 20:31:59 +0530

    problem 342

---
+++ Maths/342_power_of_four.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        //1st Approach
+        if(n==0)
+            return false;
+        while(n!=1)
+        {
+            if(n%4!=0)
+                return false;
+            n=n/4;
+        }
+        return true; 
+        //2nd Approach
+        if (n != 0 && n == (int)Math.pow(4, (Math.log(n) / Math.log(4)))) {
+            return true;
+        }
+        return false;
+    }
+}
