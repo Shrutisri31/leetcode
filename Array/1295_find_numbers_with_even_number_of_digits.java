4fc33fd34c31ddabc3e470906575cb983a753105
Shruti Srivastava <shrutishreya222@gmail.com>
2022-11-21 23:15:52 +0530

    problem 1295

---
+++ Array/1295_find_numbers_with_even_number_of_digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int tcount=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int count=0;
+            int l=nums[i];
+            while(l>0)
+            {
+                l=l/10;
+                count++;
+            }
+            if(count%2==0)
+            {
+                tcount++;
+            }
+        }
+        return tcount;
+    }
+}
