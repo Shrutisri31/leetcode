6a9db575fef152441e7db3b57ca0ea4e1ed14e13
Shruti Srivastava <shrutishreya222@gmail.com>
2023-01-15 23:53:48 +0530

    Problem 1200

---
+++ Array/1200_minimum_absolute_difference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+         Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        
+        for(int i=0; i<arr.length-1; i++){
+         if(arr[i+1] - arr[i] < min)   {
+             min = arr[i+1] - arr[i];
+         }
+        }
+        
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i+1] - arr[i] == min){               
+                List <Integer> pair = new ArrayList<>(2);
+                pair.add(arr[i]);
+                pair.add(arr[i+1]);
+                ans.add(pair);
+                
+            }
+        }
+        return ans;
+    }
+}
