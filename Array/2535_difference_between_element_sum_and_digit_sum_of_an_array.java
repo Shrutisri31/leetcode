b3d984655cfe12df45fac5bbd5d299a027358e9b
Shruti Srivastava <shrutishreya222@gmail.com>
2023-01-18 23:30:21 +0530

    Problem 2535

---
+++ Array/2535_difference_between_element_sum_and_digit_sum_of_an_array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum=0,digit=0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum + nums[i];
+        }
+         for (int i : nums) 
+        {
+            int n=i;
+            int rem=0;
+            while(n>0)
+            {
+                rem=n%10;
+                digit=digit+rem;
+                n=n/10;
+            }
+        }
+        return Math.abs(sum-digit);
+    }
+}
