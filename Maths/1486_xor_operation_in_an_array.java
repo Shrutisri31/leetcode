3520c2ef890684c5a5875b0654fa52637ee6659d
Shruti Srivastava <shrutishreya222@gmail.com>
2023-01-26 23:32:29 +0530

    problem 1486

---
+++ Maths/1486_xor_operation_in_an_array.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int xorOperation(int n, int start) {
+        int value=0; 
+        int result=0; 
+        for(int i=0; i<n; i++){  
+            value= start+ 2*i;
+            result^=value; 
+
+        }
+        return result; 
+    }
+}
