bd1c139b152ffdb56daf7392652ba8c79850fe63
Shruti Srivastava <shrutishreya222@gmail.com>
2023-02-09 23:38:22 +0530

    problem 633

---
+++ Maths/633_sum_of_square_numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int start=0,end=(int)Math.sqrt(c);
+        while(start<=end)
+        {
+           int ans=(start*start)+(end*end);
+            if(ans<0)
+            {
+                end--;
+            }
+            if(ans==c)
+            {
+                return true;
+            }
+             if(ans<c)
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+        return false;
+    }
+}
