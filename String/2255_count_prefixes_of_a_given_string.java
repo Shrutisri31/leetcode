591ef70a5daa002d09c6b9fdfca6ede55b0758bf
Shruti Srivastava <shrutishreya222@gmail.com>
2023-02-14 23:31:28 +0530

    problem 2255

---
+++ String/2255_count_prefixes_of_a_given_string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPrefixes(String[] words, String s) {
+        int result = 0;
+        for (int i=0; i<=s.length(); i++) {
+            String x = s.substring(0, i);
+            for (int j = 0; j<words.length; j++) {
+                if (x.equals(words[j]))
+                    result++;
+            }
+        }
+        return result;
+    }
+}
