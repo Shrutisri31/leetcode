e9414d6e471d98ba6bffb952846f9517b9b75d22
Shruti Srivastava <shrutishreya222@gmail.com>
2023-03-22 23:36:11 +0530

    problem 2000

---
+++ String/2000_reverse_prefix_of_word.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+         char[] array = word.toCharArray();
+        int i = 0;
+        int j = word.length();
+        int index = 0;
+        while (i < j) {
+            if (array[i] == ch) {
+                index = i;
+                break;
+            }
+            i++;
+        }
+        boolean b = index != 0;
+        if (b) {
+            int x = 0;
+            int y = array.length;
+            while (x < index) {
+                char temp = array[x];
+                array[x] = array[index];
+                array[index] = temp;
+                x++;
+                index--;
+            }
+            return(String.valueOf(array));
+        } else {
+            return(word);
+        }
+    }
+}
