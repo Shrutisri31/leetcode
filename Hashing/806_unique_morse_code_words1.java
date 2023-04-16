fb759eedac4fb7104895c70eab931603ff88198f
Shruti Srivastava <shrutishreya222@gmail.com>
2023-04-17 00:39:55 +0530

    Problem 806

---
+++ Hashing/806_unique_morse_code_words1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+     String[] MORSE_CODE = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> transformations = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(MORSE_CODE[c - 'a']);
+            }
+            transformations.add(sb.toString());
+        }
+        return transformations.size();
+
+    }
+}
