eb1a9656f9468cd681a8dce1ba1a89663ac20ad7
Shruti Srivastava <shrutishreya222@gmail.com>
2023-04-17 23:40:30 +0530

    Problem 1431

---
+++ JavaCollectionFramework/1431_kids-with-the-greatest-number-of-candies1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) max = candies[i];
+        }
+
+        ArrayList<Boolean> list = new ArrayList<>();
+
+        for (int i = 0; i < candies.length; i++) {
+            list.add(candies[i] + extraCandies >= max);
+        }
+        return list;
+        
+    }
+}
