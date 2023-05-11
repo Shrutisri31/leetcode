feb8f70dc05707ed38d37f903f4b2d687d73e63d
Shruti Srivastava <shrutishreya222@gmail.com>
2023-05-12 03:16:58 +0530

    Problem 54

---
+++ 2D_Array/54_spiral_matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+         ArrayList<Integer> list = new ArrayList<>();
+         int row = matrix.length;
+         int col = matrix[0].length;
+         int count = 0;
+         int total = row * col;
+         int startrow = 0;
+         int startcol = 0;
+         int endrow = row-1;
+         int endcol = col-1;
+         while(count < total){
+             //print starting row 
+             for(int i=startcol; count<total && i<=endcol; i++){
+                 list.add(matrix[startrow][i]);
+                 count++;
+             }
+             startrow++;
+             //print ending column 
+             for(int i=startrow; count<total && i<=endrow; i++){
+                 list.add(matrix[i][endcol]);
+                 count++;
+             }
+             endcol--;
+             //print ending row
+             for(int i=endcol; count<total && i>=startcol; i--){
+                 list.add(matrix[endrow][i]);
+                 count++;
+             }
+             endrow--;
+             //print strating column 
+             for(int i=endrow; count<total && i>=startrow;i--){
+                 list.add(matrix[i][startcol]);
+                 count++;
+             }
+             startcol++;
+         }
+         return list;
+    }
+}
