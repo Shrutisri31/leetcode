ce000fc1d76fae88630669c46461403c44bd170f
Shruti Srivastava <shrutishreya222@gmail.com>
2023-05-22 23:15:51 +0530

    Problem 1464

---
+++ Sorting_and_Searching/1464_maximum_product_of_two_elements_in_an_array.java
@@ -0,0 +1,22 @@
+//Approach 1 
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+    }
+}
+//Approach 2 
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<=nums.length; i++){
+            for(int j=i+1; j<=nums.length-1;j++){
+              int temp =  (nums[i]-1)*(nums[j]-1);
+               if(temp > max){
+                   max = temp;
+               }
+            }
+        }
+        return max;
+    }
+}
