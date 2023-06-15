10b41aa59d90aeab73cf1e4b50f6e756af7528fe
Shruti Srivastava <shrutishreya222@gmail.com>
2023-06-15 23:40:38 +0530

    Problem 17

---
+++ String/17_letter_combinations_of_a_phone_number.java
@@ -0,0 +1,37 @@
+class Solution {
+    private static void backtrack(List<String> result, Map<Character, String> digitToLetters, String digits, String current, int index) {
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+        
+        char digit = digits.charAt(index);
+        String letters = digitToLetters.get(digit);
+        
+        for (int i = 0; i < letters.length(); i++)
+         {
+            char letter = letters.charAt(i);
+            backtrack(result, digitToLetters, digits, current + letter, index + 1);
+         }
+    }
+    public List<String> letterCombinations(String digits) {
+         List<String> result = new ArrayList<>();
+          if (digits == null || digits.length() == 0)
+           {
+            return result;
+           }
+        
+        HashMap<Character, String> digitToLetters = new HashMap<>();
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+        
+        backtrack(result, digitToLetters, digits, "", 0);
+        return result;
+    }
+}
