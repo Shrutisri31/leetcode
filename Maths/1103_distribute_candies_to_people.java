6d1594c6a347586353eff93a7cebcbc8a12196bf
Shruti Srivastava <shrutishreya222@gmail.com>
2023-06-23 23:03:58 +0530

    Problem 1103

---
+++ Maths/1103_distribute_candies_to_people.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] arr = new int[num_people];
+        int idx = 0;
+        while (candies > 0) {
+            arr[idx++ % num_people] += Math.min(idx, candies);
+                candies = Math.max(candies - idx, 0);
+        }
+        return arr;
+    }
+}
