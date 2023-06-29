f047d21dd84e61f6874c67121d3975c5b0e569fa
Shruti Srivastava <shrutishreya222@gmail.com>
2023-06-29 23:30:23 +0530

    Problem 1399

---
+++ Maths/1399_count_largest_group.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countLargestGroup(int n) {
+    int[] map = new int[37];
+    int maxCount = 0, res = 0;
+    for (int i = 1; i <= n; i++) {
+      int num = i, sum = 0;
+      while (num > 0) {
+        sum += num %10;
+        num/=10;
+      }
+
+      ++map[sum];
+      if (maxCount < map[sum]) {
+        maxCount = map[sum];
+        res = 1;
+      } else if (maxCount == map[sum]) {
+        res++;
+      }
+    }
+    return res;
+    }
+}
