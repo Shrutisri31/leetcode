72524690bbcbca9e9f097e5f4d5c40da0643217c
Shruti Srivastava <shrutishreya222@gmail.com>
2023-07-27 23:30:13 +0530

    Problem 227

---
+++ Stacks/227_basic_calculator_ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calculate(String s) {
+        if(s==null || s.length()==0)
+        {
+             return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int curr = 0;
+        char op = '+';
+        char[] c = s.toCharArray();
+        for(int i = 0; i<c.length; i++){
+            if(Character.isDigit(c[i])){
+                curr = curr * 10 + c[i]-'0';
+            }
+            if(!Character.isDigit(c[i]) && c[i] != ' ' || i == c.length-1){
+                if(op=='+') stack.push(curr);
+                else if(op=='-') stack.push(-curr);
+                else if(op=='*') stack.push(stack.pop()*curr);
+                else if(op=='/') stack.push(stack.pop()/curr);
+                op = c[i];
+                curr = 0;
+            }
+        }
+        // Summing up all the values
+        int sum = 0;
+        while(!stack.isEmpty())
+            sum += stack.pop();
+        return sum;
+    }
+}
