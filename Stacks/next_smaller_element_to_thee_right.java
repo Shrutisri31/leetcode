054d5f4d651c7b2ca6143471f4660d283c78b100
Shruti Srivastava <shrutishreya222@gmail.com>
2023-07-29 23:57:21 +0530

    Problem 6

---
+++ Stacks/next_smaller_element_to_thee_right.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n){
+        // Write your code here.
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> s = new Stack<>();
+        for(int i=arr.size()-1 ; i>=0 ; i--){
+            while(!s.isEmpty() && s.peek()>=arr.get(i)){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                res.add(-1);
+            }
+            else{
+                res.add(s.peek());
+            }
+            s.push(arr.get(i));
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
