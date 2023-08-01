20173fdcbb6ea2d7274e6c2af97816f3fab10b3a
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-01 23:48:55 +0530

    Problem 349

---
+++ Array/349_intersection_of_two_arrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+      HashSet<Integer> x = new HashSet<>();
+      HashSet<Integer> y = new HashSet<>();
+           for(int i=0;i<nums1.length;i++)
+            {
+              x.add(nums1[i]);
+            }
+           for(int i=0;i<nums2.length;i++)
+           {
+              y.add(nums2[i]);
+           }
+         x.retainAll(y);
+         int ans[]=new int[x.size()];
+         int i = 0;
+           for(Integer n : x) 
+           {
+              ans[i] = n;
+               i++;
+           }
+     return ans; 
+    }
+}
