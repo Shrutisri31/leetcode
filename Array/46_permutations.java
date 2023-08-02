51afdbcef7921c2f6dbda97e2bbab16c5732eb85
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-02 23:51:55 +0530

    Problem 46

---
+++ Array/46_permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void backtrack(int[] nums, List<Integer> currPm, List<List<Integer>> res)
+     {
+        if (currPm.size() == nums.length) {
+            res.add(new ArrayList<>(currPm));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (currPm.contains(nums[i])) {
+                continue; // Skip if the element is already in the current permutation
+            }
+
+            currPm.add(nums[i]);
+            backtrack(nums, currPm, res);
+            currPm.remove(currPm.size() - 1);
+        }
+    }
+}
