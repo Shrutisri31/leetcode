195b260dfdf6da1df5e3160bf56664f7727ec930
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-09 20:11:14 +0530

    Problem 114

---
+++ Tree/114_flatten_binary_tree_to_linked_list.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null || root.left == null && root.right == null){
+            return;
+        }
+
+        if(root.left!=null)
+        {
+        flatten(root.left);
+        TreeNode temp = root.right;
+        root.right=root.left;
+        root.left = null;
+
+        TreeNode t = root.right; //firstly it was root.left
+        while(t.right!=null){
+            t = t.right;
+        }
+        t.right = temp;
+        }
+        flatten(root.right);
+    }
+}
