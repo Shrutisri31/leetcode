ddb6eee91e7a946cd6708cf5ba364cd2ab93d275
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-12 23:31:11 +0530

    Problem 258

--- Maths/258_add_digits.java
+++ Maths/258_add_digits.java
@@ -1,13 +1,11 @@
 class Solution {
     public int addDigits(int num) {
-           if (num / 10 == 0) {
-                 return num;
-               }
-    int digitSum = 0;
-    while (num > 0) {
-      digitSum += num % 10;
-      num /= 10;
-    }
-    return addDigits(digitSum);
+           if (num / 10 == 0)  return num;         
+           int digitSum = 0;
+           while (num > 0) {
+              digitSum += num % 10;
+              num /= 10;
+           }
+       return addDigits(digitSum);
     }
 }
