1b79c2c79b6af518fd96bcbaaa2915713e8a7f1c
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-16 23:49:50 +0530

    Problem 53

--- Array/53_maximum_subarray.java
+++ Array/53_maximum_subarray.java
@@ -4,12 +4,8 @@ class Solution {
         int max = nums[0];
         for(int i=0;i<nums.length;i++){
             sum = sum + nums[i];
-            if(sum>max){
-                max = sum;
-            }
-            if(sum<0){
-                sum =0;
-            }
+            if(sum>max)  max = sum;     
+            if(sum<0)  sum =0;
         }
         return max;
     }
