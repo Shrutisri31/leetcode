079e9c51ff437f8934616096a7f6328794b87194
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-18 23:25:03 +0530

    Problem 412

--- Maths/412_fizz_buzz.java
+++ Maths/412_fizz_buzz.java
@@ -4,15 +4,9 @@ class Solution {
         for(int num=1;num<=n;num++){
             boolean divisibleby3=(num%3==0);
             boolean divisibleby5=(num%5==0);
-            if(divisibleby3 && divisibleby5){
-                ans.add("FizzBuzz");
-            }
-            else if(divisibleby3){
-                ans.add("Fizz");
-            }
-            else if(divisibleby5){
-                ans.add("Buzz");
-            }
+            if(divisibleby3 && divisibleby5)   ans.add("FizzBuzz");    
+            else if(divisibleby3) ans.add("Fizz");  
+            else if(divisibleby5) ans.add("Buzz");
             else{
                 ans.add(Integer.toString(num));
             }
