b0c0c6db49b9f4bb593b168686f961c23a4e39e5
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-23 23:34:41 +0530

    Problem 735

--- Stacks/735_asteroid_collision.java
+++ Stacks/735_asteroid_collision.java
@@ -1,24 +1,19 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
        Stack<Integer> s = new Stack<Integer>();
-
         for (int asteroid : asteroids)
         {            
             while(!s.isEmpty() && asteroid<0 && s.peek()>0 && s.peek()<Math.abs(asteroid))
-                s.pop(); 
-            
-
+                s.pop();  
             if (s.isEmpty() || asteroid > 0 || s.peek() < 0) 
                 s.push(asteroid);
             else if (s.peek() == Math.abs(asteroid))
                 s.pop();
         }
-
         int[] res = new int[s.size()];
         for (int i = s.size() - 1; i >= 0; i--) {
             res[i] = s.pop();
         }
-
         return res;
     }
 }
