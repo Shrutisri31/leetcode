a5723f92a438abefe7b9c6df15e69500196064b6
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-30 23:16:43 +0530

    Problem 509

--- Maths/509_fibonacci_number.java
+++ Maths/509_fibonacci_number.java
@@ -1,16 +1,23 @@
 class Solution {
     public int fib(int n) {
-         if(n <= 1)
-         {
-             return n;
-         }
-		int a = 0, b = 1;
-		while(n-- > 1)
-		{
-			int sum = a + b;
-			a = b;
-			b = sum;
-		}
-        return b;
+      public class Solution {
+
+    int fib(int n) {
+        if( n <= 0){
+            return 0;
+        }
+        else if( n == 1){
+            return 1;
+        }
+        int a=0, b=1;
+        int c = a+b;
+        for(int i=2; i<n; i++){
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
     }
+}
+}
 }
