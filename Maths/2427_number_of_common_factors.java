31226bbd37118b75fadb60c0c10afc82eb27d9fe
Shruti Srivastava <shrutishreya222@gmail.com>
2023-08-31 23:50:49 +0530

    Problem 2427

--- Maths/2427_number_of_common_factors.java
+++ Maths/2427_number_of_common_factors.java
@@ -3,7 +3,8 @@ class Solution {
         int min = Math.min(a,b);
         int commonFactors = 0;
        for(int i=1; i<=min; i++){
-         if((a%i ==0) && (b%i ==0)){
+         if((a%i ==0) && (b%i ==0))
+         {
            commonFactors++;
          }
        }
