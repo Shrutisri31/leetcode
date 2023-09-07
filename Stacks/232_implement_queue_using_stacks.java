ab59e4076951f9501b7dfc9cc0410930178d0af3
Shruti Srivastava <shrutishreya222@gmail.com>
2023-09-07 23:23:53 +0530

    Problem 232

--- Stacks/232_implement_queue_using_stacks.java
+++ Stacks/232_implement_queue_using_stacks.java
@@ -2,16 +2,13 @@
 class MyQueue {
     Stack<Integer> stack1;
     Stack<Integer> stack2;
-
     public MyQueue() {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
-    
     public void push(int x) {
        stack1.push(x);
     }
-    
     public int pop() {
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
@@ -20,7 +17,6 @@ class MyQueue {
         }
         return stack2.pop();
     }
-    
     public int peek() {
          if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
@@ -29,7 +25,6 @@ class MyQueue {
         }
         return stack2.peek();
     }
-    
     public boolean empty() {
         return stack1.isEmpty() && stack2.isEmpty();
     }
