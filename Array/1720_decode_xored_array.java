15beb4288d74367e99ca2552b78b3f45d2a5db27
Shruti Srivastava <shrutishreya222@gmail.com>
2023-09-12 23:39:28 +0530

    Problem 1720

--- Array/1720_decode_xored_array.java
+++ Array/1720_decode_xored_array.java
@@ -3,7 +3,8 @@ class Solution {
       int n = encoded.length;
         int[] dec = new int[n + 1];
         dec[0] = first;
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++)
+        {
             dec[i] = (encoded[i - 1] ^ dec[i - 1]);
         }
         return dec;  
