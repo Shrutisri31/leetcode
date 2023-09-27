e3d20b0f159eb7f06762cf917c0c7b6cb2c7b06e
Shruti Srivastava <shrutishreya222@gmail.com>
2023-09-27 23:53:30 +0530

    Problem 1523

--- Maths/1523_count_odd_numbers_in_an_interval_range.java
+++ Maths/1523_count_odd_numbers_in_an_interval_range.java
@@ -1,7 +1,8 @@
 class Solution {
     public int countOdds(int low, int high) {
         int odd = (high - low) / 2;
-        if (low % 2 == 1 || high % 2 == 1) {
+        if (low % 2 == 1 || high % 2 == 1)
+         {
             odd++;
         }
         return odd;
