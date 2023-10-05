ffef3064093376bcce8637c4e3636919771a7dc8
Shruti Srivastava <shrutishreya222@gmail.com>
2023-10-05 23:16:04 +0530

    Problem 43

--- String/43_multiply_strings.java
+++ String/43_multiply_strings.java
@@ -2,7 +2,6 @@ class Solution {
     public String multiply(String num1, String num2) {
     int m = num1.length(), n = num2.length();
     int[] pos = new int[m + n];
-   
     for(int i = m - 1; i >= 0; i--) {
         for(int j = n - 1; j >= 0; j--) {
             int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
@@ -13,7 +12,6 @@ class Solution {
             pos[p2] = (sum) % 10;
         }
     }  
-    
     StringBuilder sb = new StringBuilder();
     for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
     return sb.length() == 0 ? "0" : sb.toString();
