417b5bcdad852d67520a943585cfcd81d04a7b02
Shruti Srivastava <shrutishreya222@gmail.com>
2023-10-26 23:06:59 +0530

    Problem 206

--- LinkedList/206_reverse_linked_list.java
+++ LinkedList/206_reverse_linked_list.java
@@ -1,7 +1,8 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         //check if LL is empty or single node 
-        if(head == null || head.next == null){
+        if(head == null || head.next == null)
+        {
             return head;
         }
         //recursive approach
