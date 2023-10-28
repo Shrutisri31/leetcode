1fe853e27f09f4811d48cbf7ae7a8f14eb645f5b
Shruti Srivastava <shrutishreya222@gmail.com>
2023-10-28 22:02:40 +0530

    Problem 2148

--- Array/2148_count_elements_with_strictly_smaller_and_greater_elements.java
+++ Array/2148_count_elements_with_strictly_smaller_and_greater_elements.java
@@ -3,12 +3,14 @@ class Solution {
         Arrays.sort(nums);
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length;i++) {
+        for (int i = 0; i < nums.length;i++)
+         {
             min = Math.min(min, nums[i]);
             max = Math.max(max, nums[i]);
         }
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
+        {
             if (nums[i] > min && nums[i] < max) {
                 count++;
             }
