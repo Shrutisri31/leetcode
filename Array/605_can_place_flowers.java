2fcb8d4fddf43297f3a0a58c2b8cf817f2190a43
Shruti Srivastava <shrutishreya222@gmail.com>
2023-10-29 23:23:32 +0530

    Problem 605

--- Array/605_can_place_flowers.java
+++ Array/605_can_place_flowers.java
@@ -3,8 +3,7 @@ class Solution {
          if (n == 0) {
             return true;
         }
-        for (int i = 0; i < flowerbed.length; i++)
-         {
+        for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
                 flowerbed[i] = 1;
                 n--;
