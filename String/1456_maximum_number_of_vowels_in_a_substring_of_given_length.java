90fad76ac181bbc4c12af3eee7f7d79bea7b0bf6
Shruti Srivastava <shrutishreya222@gmail.com>
2023-11-01 23:22:37 +0530

    Problem 1456

--- String/1456_maximum_number_of_vowels_in_a_substring_of_given_length.java
+++ String/1456_maximum_number_of_vowels_in_a_substring_of_given_length.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxVowels(String s, int k) {
         int maxCount = 0;
         int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i >= k) {
+        for (int i = 0; i < s.length(); i++){
+            if (i >= k){
                 char firstChar = s.charAt(i - k);
-                if (isVowel(firstChar)) {
+                if (isVowel(firstChar)){
                     count--;
                 }
             }
