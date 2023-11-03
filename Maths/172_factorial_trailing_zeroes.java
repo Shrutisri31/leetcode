b5d5a54f4f499ed24bcb62b1297b7883d76b1e97
Shruti Srivastava <shrutishreya222@gmail.com>
2023-11-03 23:27:40 +0530

    Problem 172

--- Maths/172_factorial_trailing_zeroes.java
+++ Maths/172_factorial_trailing_zeroes.java
@@ -1,10 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
         int count=0;
-        while(n!=0)
-        {
-            n=n/5;
-            count=count+n;
+        while(n!=0) {
+            n = n/5;
+            count = count + n;
         }
         return count;
     }
