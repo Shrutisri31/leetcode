f3ed2dfc67c23923a4a286fb8751566a066ccb94
Shruti Srivastava <shrutishreya222@gmail.com>
2023-11-12 22:38:09 +0530

    Problem 2586

--- String/2586_count_the_number_of_vowel_strings_in_range.java
+++ String/2586_count_the_number_of_vowel_strings_in_range.java
@@ -1,17 +1,17 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
-        HashSet<Character> hs=new HashSet<>();
-        hs.add('a');
-        hs.add('e');
-        hs.add('i');
-        hs.add('o');
-        hs.add('u');
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
         int count=0;
         for(int i=left;i<=right;i++){
             String s=words[i];
             char ch1=s.charAt(0);
             char ch2=s.charAt(s.length()-1);
-            if(hs.contains(ch1) && hs.contains(ch2))
+            if(set.contains(ch1) && set.contains(ch2))
                 count++;
         }
         return count;
