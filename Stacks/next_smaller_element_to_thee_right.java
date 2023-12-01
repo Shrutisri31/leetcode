25d38da48adb252b0af7b39c5639f6c1ca552206
Shruti Srivastava <shrutishreya222@gmail.com>
2023-12-01 23:38:22 +0530

    Problem 112

--- Stacks/next_smaller_element_to_thee_right.java
+++ Stacks/next_smaller_element_to_thee_right.java
@@ -7,7 +7,8 @@ public class Solution{
         ArrayList<Integer> res = new ArrayList<>();
         Stack<Integer> s = new Stack<>();
         for(int i=arr.size()-1 ; i>=0 ; i--){
-            while(!s.isEmpty() && s.peek()>=arr.get(i)){
+            while(!s.isEmpty() && s.peek()>=arr.get(i))
+            {
                 s.pop();
             }
             if(s.isEmpty()){
