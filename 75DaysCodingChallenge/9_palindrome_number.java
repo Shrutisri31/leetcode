2de9011bc2cbf8911af856f318688b2b88d09dd2
Shruti Srivastava <shrutishreya222@gmail.com>
2023-12-06 00:10:18 +0530

    Problem 9

---
+++ 75DaysCodingChallenge/9_palindrome_number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int temp = x;
+       int rem = 0, rev=0;
+       while(temp > 0){
+          rem = temp % 10;
+          rev = rev * 10 + rem;
+          temp = temp/10;
+       }
+       if(rev == x){
+           return true;
+       }
+       return false;
+    }
+}
