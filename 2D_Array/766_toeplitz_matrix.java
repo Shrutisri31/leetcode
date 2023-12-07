6f6fa96c9b2523478b0fc0f192acb0a90884a39d
Shruti Srivastava <shrutishreya222@gmail.com>
2023-12-07 23:37:08 +0530

    Problem 766

--- 2D_Array/766_toeplitz_matrix.java
+++ 2D_Array/766_toeplitz_matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isToeplitzMatrix(int[][] matrix) {
+    public boolean isToeplitzMatrix(int[][] matrix){
          for(int i=1;i<matrix.length;i++){
             for(int j=1;j<matrix[0].length;j++){
                 if(matrix[i-1][j-1]!=matrix[i][j]){
