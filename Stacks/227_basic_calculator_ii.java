eda066d75bcfd8044aa4f39305538c171edeb9db
Shruti Srivastava <shrutishreya222@gmail.com>
2023-12-27 22:54:02 +0530

    Problem 227

--- Stacks/227_basic_calculator_ii.java
+++ Stacks/227_basic_calculator_ii.java
@@ -1,7 +1,6 @@
 class Solution {
     public int calculate(String s) {
-        if(s==null || s.length()==0)
-        {
+        if(s==null || s.length()==0){
              return 0;
         }
         Stack<Integer> stack = new Stack<>();
