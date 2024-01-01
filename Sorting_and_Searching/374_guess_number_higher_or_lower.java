20f43acab4696a60c48bf5e7bfe907a7c69124f7
Shruti Srivastava <shrutishreya222@gmail.com>
2024-01-01 23:03:41 +0530

    Problem 374

--- Sorting_and_Searching/374_guess_number_higher_or_lower.java
+++ Sorting_and_Searching/374_guess_number_higher_or_lower.java
@@ -8,7 +8,7 @@
  */
 
 public class Solution extends GuessGame {
-    public int guessNumber(int n) {
+    public int guessNumber(int n){
         int start = 0, end = n;
         while(start <= end){
             int mid = start + (end - start)/2;
