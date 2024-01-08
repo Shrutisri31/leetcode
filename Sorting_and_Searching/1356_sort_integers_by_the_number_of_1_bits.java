5df2bb666b63bcecb4bb0f1d42638e618d9de3ed
Shruti Srivastava <shrutishreya222@gmail.com>
2024-01-08 22:20:51 +0530

    Problem 1356

--- Sorting_and_Searching/1356_sort_integers_by_the_number_of_1_bits.java
+++ Sorting_and_Searching/1356_sort_integers_by_the_number_of_1_bits.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] sortByBits(int[] arr) {   
-        for(int i=0;i<arr.length;i++)
-        {
+        for(int i=0;i<arr.length;i++){
             arr[i]+=Integer.bitCount(arr[i])*10001;
         }  
         Arrays.sort(arr); 
