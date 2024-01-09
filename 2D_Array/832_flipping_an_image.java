3d2a4196deb6aa52a2fdc2247d50b41df699d42b
Shruti Srivastava <shrutishreya222@gmail.com>
2024-01-10 00:10:32 +0530

    Problem 832

--- 2D_Array/832_flipping_an_image.java
+++ 2D_Array/832_flipping_an_image.java
@@ -4,7 +4,7 @@ class Solution {
         int col = image[0].length;
         int[][] res = new int[row][col];
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+            for (int j = 0; j < col; j++){
                 res[i][j] = image[i][col-j-1];
             }
         }
