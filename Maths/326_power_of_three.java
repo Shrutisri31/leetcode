9ff1dc3fe3ffccda74de3eb660e6f0b598b79d30
Shruti Srivastava <shrutishreya222@gmail.com>
2024-01-16 23:28:53 +0530

    Problem 326

--- Maths/326_power_of_three.java
+++ Maths/326_power_of_three.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n==0)
+        if(n==0){
             return false;
+        }      
             while(n!=1)
             {
                 if(n%3!=0)
