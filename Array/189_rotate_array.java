f2e499a98b5d90dbd606efa76d4ef2ee816b04c6
Shruti Srivastava <shrutishreya222@gmail.com>
2024-02-06 22:42:21 +0530

    Problem 189

--- Array/189_rotate_array.java
+++ Array/189_rotate_array.java
@@ -1,8 +1,7 @@
 class Solution {
     public static void reverse(int nums[], int start, int end)
     {
-        while(start < end)
-        {
+        while(start < end){
           int temp = nums[start];
           nums[start] = nums[end];
           nums[end] = temp;
