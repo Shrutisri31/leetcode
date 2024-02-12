c80f05bfb7355ea13a291012691ff17c95c7f977
Shruti Srivastava <shrutishreya222@gmail.com>
2024-02-12 23:05:21 +0530

    Problem 268

--- Array/268_missing-number.java
+++ Array/268_missing-number.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int missingNumber(int[] nums)
-    {
+    public int missingNumber(int[] nums){
        int n = nums.length;
        int totalSum = n*(n+1)/2;
         int sum = 0;
