6a868d183ae1cc5af3cf2abaafaf85c3c5bb58db
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-02 22:27:54 +0530

    Problem 1

---
+++ Array/two_sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         
+        int res[]=new int[2];
+        res[0]=0; res[1]= 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+          int num = nums[i];
+          int more = target - num;
+          if(map.containsKey(more)){
+              res[0] = map.get(more);
+              res[1] = i;
+              return res;
+          }
+          map.put(nums[i],i);
+        }
+        return res;
+    }
+}
