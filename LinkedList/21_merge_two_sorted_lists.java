2fd7207a469be1fa92ab3adcc4540fc0c37df942
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-06 23:34:11 +0530

    Problem 21

--- LinkedList/21_merge_two_sorted_lists.java
+++ LinkedList/21_merge_two_sorted_lists.java
@@ -12,7 +12,6 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode prehead = new ListNode(-1);
         ListNode cur = prehead;
-
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
                 cur.next = list1;
