c1c03efb28afcd478b526f967bd03079bc579ecc
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-09 23:04:46 +0530

    Problem 628

--- Array/628_maximum_product_of_three_numbers.java
+++ Array/628_maximum_product_of_three_numbers.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maximumProduct(int[] nums) {
+    public int maximumProduct(int[] nums){
         int n=nums.length;
         Arrays.sort(nums);
         int r = nums[n-1] * nums[n-2] * nums[n-3]; 
