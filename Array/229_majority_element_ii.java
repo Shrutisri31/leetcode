a54ad67066746b04ed70fdf44c91412cc14424ac
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-11 23:11:36 +0530

    Problem 229

--- Array/229_majority_element_ii.java
+++ Array/229_majority_element_ii.java
@@ -2,23 +2,18 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int ans1=Integer.MIN_VALUE , ans2 = Integer.MIN_VALUE;
         int count1 = 0 , count2 = 0;
-        for(int e: nums)
-        {
-            if(e == ans1)
-            {
+        for(int e: nums){
+            if(e == ans1) {
                 count1++;
             }
-            else if( e == ans2)
-            {
+            else if( e == ans2){
                 count2++;
             }
-            else if (count1 == 0)
-            {
+            else if (count1 == 0){
                 ans1 = e;
                 count1=1;
             }
-            else if(count2 == 0)
-            {
+            else if(count2 == 0){
                 ans2 = e;
                 count2 = 1;
             }
@@ -29,25 +24,20 @@ class Solution {
         }
             //checking if ans1 and ans2 is actually the answer 
             count1 = 0 ; count2 = 0;
-            for(int e:nums)
-            {
-                if(e == ans1)
-                {
+            for(int e:nums){
+                if(e == ans1){
                     count1++;
                 }
-                else if(e == ans2)
-                {
+                else if(e == ans2){
                     count2++;
                 }
             }
             ArrayList<Integer> res = new ArrayList<>();
 
-            if(count1 > nums.length/3)
-            {
+            if(count1 > nums.length/3){
                 res.add(ans1);
             }
-           if(count2 > nums.length/3)
-           {
+           if(count2 > nums.length/3){
                 res.add(ans2);
            }
         return res;
