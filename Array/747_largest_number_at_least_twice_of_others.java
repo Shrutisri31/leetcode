cca96a60bbb6a05d40580000491f1f6a29a35c82
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-14 23:47:28 +0530

    Problem 747

--- Array/747_largest_number_at_least_twice_of_others.java
+++ Array/747_largest_number_at_least_twice_of_others.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int dominantIndex(int[] nums) {
+    public int dominantIndex(int[] nums){
         int result = -1, tmp = -1, ans = -1;
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] > result) {
+        for (int i = 0; i < nums.length; ++i){
+            if (nums[i] > result){
                 tmp = result;
                 result = nums[i];
                 ans = i;
