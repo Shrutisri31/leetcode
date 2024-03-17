10a43aecb86e23297016ccce1286f5b7a3b74c34
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-17 23:37:42 +0530

    Problem 724

--- Array/724_find_pivot_index.java
+++ Array/724_find_pivot_index.java
@@ -1,13 +1,10 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int sumL=0,sumR=0 , totalsum=0;
-       
-        for(int i=0;i<nums.length;i++)
-        {
+        for(int i=0;i<nums.length;i++){
             totalsum+=nums[i];
         }
-        for(int i=0 ; i<nums.length ; i++)
-        {
+        for(int i=0 ; i<nums.length ; i++){
             sumR=totalsum-sumL;
             if((sumR-nums[i])==sumL){   
                 return i;
