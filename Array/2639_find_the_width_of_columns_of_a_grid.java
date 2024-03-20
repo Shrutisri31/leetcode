3936a65a182431261f7fb9bb7a48369e27285270
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-20 22:47:22 +0530

    Problem 2639

--- Array/2639_find_the_width_of_columns_of_a_grid.java
+++ Array/2639_find_the_width_of_columns_of_a_grid.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] findColumnWidth(int[][] grid) {
        int C = grid[0].length;
         int[] ans = new int[C];
-        for (int c = 0, R = grid.length; c < C; ++c) {
+        for (int c = 0, R = grid.length; c < C; ++c)
+        {
             for (int r = 0; r < R; ++r) {
                 ans[c] = Math.max(ans[c], Integer.toString(grid[r][c]).length());
             }
