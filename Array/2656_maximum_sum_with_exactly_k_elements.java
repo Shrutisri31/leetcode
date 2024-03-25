777f4c0e419b1f9c55ad919df6bcbb00623d9d6c
Shruti Srivastava <shrutishreya222@gmail.com>
2024-03-25 21:59:12 +0530

    Problem 2656

--- Array/2656_maximum_sum_with_exactly_k_elements.java
+++ Array/2656_maximum_sum_with_exactly_k_elements.java
@@ -1,8 +1,7 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
        int max = nums[0];
-        for(int i=1; i<nums.length; i++)
-        {
+        for(int i=1; i<nums.length; i++){
             if(max<nums[i]) max = nums[i];
         }
         return max*k + (k-1)*k/2; 
